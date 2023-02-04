cc2af250694146922dc9fb0a96e284fcc9663800
unknown <prohanter34@gmail.com>
2023-02-04 15:48:14 +0300

    artem

--- square.py
+++ square.py
@@ -1,7 +1,12 @@
-a=int(input())
-b=int(input())
-c=int(input())
-d=b*b-4*a*c
-x1=(b-d**(1/2))/(2*a)
-x2=(b+d**(1/2))/(2*a)
+a = int(input())
+b = int(input())
+c = int(input())
+d = b*b-4*a*c
+x1 = (b-d**(1/2))/(2*a)
+x2 = (b+d**(1/2))/(2*a)
 print(x1, x2)
+
+
+
+
+
