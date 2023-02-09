c9e6a09ba6a30fa521fd92eda9d0bcacda2a7bc5
unknown <prohanter34@gmail.com>
2023-02-09 09:52:28 +0300

    last

--- square.py
+++ square.py
@@ -2,9 +2,16 @@ a = int(input())
 b = int(input())
 c = int(input())
 d = b*b-4*a*c
-x1 = (b-d**(1/2))/(2*a)
-x2 = (b+d**(1/2))/(2*a)
-print(x1, x2)
+if d >0:
+    x1 = (b-d**(1/2))/(2*a)
+    x2 = (b+d**(1/2))/(2*a)
+    print(x1, x2)
+elif d==0:
+    x1 = (b - d ** (1 / 2)) / (2 * a)
+    print(x1)
+else:
+    print("Корней нет")
+
 
 
 
