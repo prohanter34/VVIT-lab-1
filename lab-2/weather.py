a4daae747f2fbbdcdd8891dd39280df9ea1a209b
karavaichek <prohanter34@gmail.com>
2023-05-24 23:13:31 +0300

    name

---
+++ lab-2/weather.py
@@ -0,0 +1,26 @@
+import requests
+apiKey = "c16a1d436e8a37d630f58618776b0216"
+city = "Moscow"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+             params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': apiKey})
+data=res.json()
+print("Прогноз на день")
+print("Город:", city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+print("Видимость:", data["visibility"])
+print("Скорость ветра:", data["wind"]["speed"], " \r\n")
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+             params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': apiKey})
+data = res.json()
+for i in data["list"]:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']),
+          "> \r\nПогодные условия <", i['weather'][0]['description'], ">" "> \r\n Скорость ветра <", i['wind']["speed"], ">" 
+          "> \r\n Видимость <", i['visibility'], ">" )
+    print("____________________________")
+
+
+
